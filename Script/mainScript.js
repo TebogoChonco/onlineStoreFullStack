69cb70d07c1d634dd452fbf1a3d95f5aaae06fda
Tebogochonco <115028731+Tebogochonco@users.noreply.github.com>
2023-09-22 01:23:33 +0200

    Update
    Reverted back to the original project. Removed the supabase files and added php and sql as it is a database I'm better familiar with

--- Script/mainScript.js
+++ Script/mainScript.js
@@ -1,7 +1,3 @@
-import '../style.css'
-
-import { supabase } from "../supabase";
-
 let shop = document.getElementById("shop");
 
 /**
@@ -9,14 +5,10 @@ let shop = document.getElementById("shop");
  * ? the getItem part is retrieving data from the local storage
  * ? if local storage is blank, basket becomes an empty array
  */
-const useData = async () => {
-  const { data, error } = await supabase.from("Products").select(); // gets the data from supabase
-  return data;
-};
 
-//let basket = JSON.parse(localStorage.getItem("data")) || [];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-let generateShop = async() => {
+let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
       let { id, name, desc, img, price } = x;
@@ -107,36 +99,3 @@ let calculation = () => {
 };
 
 calculation();
-
-/////////////////////////////////
-
-async function renderVegetables() {
-  const vegetables = await useData();
-  console.log(vegetables);
-
-  const vegetablesContainer = document.getElementById('vegetablesContainer');
-
-  vegetables.forEach((vegetable) => {
-    console.log(vegetable);
-    const vegetableElement = document.createElement('div');
-    vegetableElement.classList.add('vegetable-item'); // You can add a CSS class for styling.
-
-    // Generate the HTML content for the vegetable.
-    vegetableElement.innerHTML = `
-      <h2>${vegetable.productName}</h2>
-      <p>${vegetable.productDescr}</p>
-      <!-- Add more properties as needed -->
-    `;
-
-    // Append the vegetable element to the container.
-    vegetablesContainer.appendChild(vegetableElement);
-  });
-}
-
-// Call the function to render the vegetables
-renderVegetables();
-
-// ...rest of your code
-
-
-
